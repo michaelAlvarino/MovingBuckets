3992dd27cecbd0bcad14a9f9afd8c4bb087f617a
Michael Alvarino <michael.alvarino@gmail.com>
2016-11-01 11:10:12 -0400

    add true solution

--- Solution.hs
+++ Solution.hs
@@ -3,3 +3,12 @@ import Combinations
 import Permutations
 
 numOfWaysToMoveBuckets x = sum (map permutations (repTwos (combination x) []))
+
+-- real solution...
+-- if you draw out the first few answers, you'll notice this is just the fibonnaci sequence
+-- there's tons of ways to do this in haskell, I like zipWith and infinite lists
+
+fibs :: [Integer]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+realNumWaysToMoveBuckets x = fibs !! n
